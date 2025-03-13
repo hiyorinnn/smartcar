947c42020323c5cf282e0b1f0ea188b7d390824f
hiyorinnn <144999462+hiyorinnn@users.noreply.github.com>
2025-03-13 17:31:58 +0800

    hehe

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <router-view></router-view> <!-- This is where the routed components will be rendered -->
+    <!-- This is where the routed components will be rendered -->
 
-    <!-- Using router-link to navigate to the Payment page -->
+    <!-- Static content in the App component -->
+    <p>Hello</p>
+
+
+    <!-- Button to navigate to the Payment page via router-link -->
     <router-link to="/payment">
-      Payment
+      Go to Payment Page
     </router-link>
+
+    <router-view></router-view> 
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
+
 };
 </script>
--- my-vue-project/src/main.js
+++ my-vue-project/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';  // Import the router instance
+import router from './router/index.js';  // Import the router instance
 
 createApp(App)
   .use(router)  // Use the router
