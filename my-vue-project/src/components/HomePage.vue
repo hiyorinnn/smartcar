0a730aef2fcb2a86e98dbe554ef38659f37ddc7a
hiyorinnn <144999462+hiyorinnn@users.noreply.github.com>
2025-03-13 16:19:09 +0800

    Config v3

--- my-vue-project/src/components/HomePage.vue
+++ my-vue-project/src/components/HomePage.vue
@@ -1,3 +1,15 @@
 <template>
-  <b>Home</b>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'HomePage', // Export the component to use in other files
+};
+</script>
+
+<style scoped>
+/* Your styles here */
+</style>
